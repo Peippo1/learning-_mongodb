afe24d8ba2108e9dac7d369f4351c39c831cb8fb
Peippo <32274695+Peippo1@users.noreply.github.com>
2021-01-27 13:26:27 +0000

    mongoose > mongo db driver connected

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,18 +1,13 @@
-const { MongoClient }= require("mongodb");
-
-const URI = "mongodb+srv://username:password@cluster0.iamlm.mongodb.net/tester?retryWrites=true&w=majority";
+// const { MongoClient }= require("mongodb");
+const { mongoose } = require("mongoose");
+require("dotnv").config();
 
 const connection = async () => {
-    try {
-        const client = new MongoClient(URI, {useUnifiedTopology: true });
-        await client.connect();
-        console.log("successfully connected");
-        const db =client.db("people");
-        const collection = db.collection("emails");
-        await collection.insertOne({ email: "mrpanda@email.com"});
-
-        client.close();
-
+// asynchronous function to contain mongodb call and awaiting connection
+    try{
+        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("successfuly connected");
+        // below is title of database and the data that is going to be stored there.
     }   catch (error) {
         console.log(error);
     }
@@ -20,24 +15,27 @@ const connection = async () => {
 
 connection();
 
+
 // to run node src/db/connection.js
 
+// const { MongoClient }= require("mongodb");
 
-// const { mongoose } = require("mongoose");
-// require("dotnv").config();
+// const URI = "mongodb+srv://username:password@cluster0.iamlm.mongodb.net/tester?retryWrites=true&w=majority";
 
 // const connection = async () => {
-// asynchronous functiuon to contain mongodb call and awaiting connection
-//     try{
-//         await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-//         console.log("successfuly connected");
-//         // below is title of database and the data that is going to be stored there.
+//     try {
+//         const client = new MongoClient(URI, {useUnifiedTopology: true });
+//         await client.connect();
+//         console.log("successfully connected");
+//         const db =client.db("people");
+//         const collection = db.collection("emails");
+//         await collection.insertOne({ email: "mrpanda@email.com"});
+
+//         client.close();
+
 //     }   catch (error) {
 //         console.log(error);
 //     }
 // };
 
 // connection();
-
-
-// to run node src/db/connection.js
